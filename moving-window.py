b6b8e7b7561c5cf95c319efa35f018be8176c4e6
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2021-10-04 16:58:59 +0200

    Update

--- moving-window.py
+++ moving-window.py
@@ -9,80 +9,140 @@
 # license         :GPLv3
 # ==============================================================================
 
-# Import
-import os
+
+# Python virtual environment
+# conda create --name jnr-vcs -c conda-forge python gdal numpy
+# matplotlib pip scipy pandas --yes
 
 # Third party imports
 import numpy as np
 from osgeo import gdal
-import scipy
+import scipy.ndimage
 
 # Local application imports
-from ..misc import progress_bar
+from misc import progress_bar
 
 
-# perc_defor
-def perc_defor(input, output, win_size, blk_rows=128):
-    """Compute the percentage of deforestation using a moving window.
+# local_defor_rate
+def local_defor_rate(input_file, output_file, win_size, time_interval,
+                     blk_rows=128):
+    """Compute the local deforestation rate using a moving window.
 
-    Compute the percentage of deforestation using a moving
-    window. SciPy is used for the focal analysis. The uniform_filter
-    is used over the generic_filter. The generic_filter is 40 times
-    slower than the strides implemented in the uniform_filter.
+    Compute the local deforestation rate using a moving window. SciPy
+    is used for the focal analysis. The ``uniform_filter`` is used
+    over the ``generic_filter``. The ``generic_filter`` is 40 times
+    slower than the strides implemented in the ``uniform_filter``. For
+    cells on the edge of the raster, the local deforestation rate is
+    computed from a lower number of existing cells in the moving
+    window using ``mode='constant'`` and ``cval=0``.
 
-    :param input_file: Input raster file of forest cover change at three
-        dates (123). No data value must be 0 (zero).
+    :param input_file: Input raster file of forest cover change at
+        three dates (123). 1: first period deforestation, 2: second
+        period deforestation, 3: remaining forest at the end of the
+        second period. No data value must be 0 (zero).
+
     :param output_file: Output raster file.
-    :param win_size: Window size in number of cells. Must be an odd number.
-    :param blk_rows: Number of rows for block.
 
-    :return: A raster with the percentage of deforestation locally.
+    :param win_size: Size of the moving window in number of
+        cells. Must be an odd number lower or equal to ``blk_rows``.
+
+    :param time_interval: Time interval (in years) for forest cover
+        change observations.
+
+    :param blk_rows: Number of rows for block. Must be greater or
+        equal to ``win_size``. This is used to break lage raster files
+        in several blocks of data that can be hold in memory.
+
+    :return: A raster with the local deforestation rate.
 
     """
 
+    # Check win_size
+    if (win_size % 2) == 0:
+        print("'win_size' must be an odd number.")
+        return None
+    if (win_size > blk_rows):
+        print("'win_size' must be lower or equal to 'blk_rows'.")
+        return None
+
+    # Get raster data
     in_ds = gdal.Open(input_file)
     in_band = in_ds.GetRasterBand(1)
-
+    # Raster size
     xsize = in_band.XSize
     ysize = in_band.YSize
 
+    # Create output raster file
     driver = gdal.GetDriverByName('GTiff')
     out_ds = driver.Create(output_file, xsize, ysize, 1,
-                           gdal.GDT_Int32,
+                           gdal.GDT_UInt16,
                            ["COMPRESS=LZW", "PREDICTOR=2", "BIGTIFF=YES"])
     out_ds.SetProjection(in_ds.GetProjection())
     out_ds.SetGeoTransform(in_ds.GetGeoTransform())
     out_band = out_ds.GetRasterBand(1)
-    out_band.SetNoDataValue(-9999)
+    out_band.SetNoDataValue(65535)
 
+    # Iteration
+    iter_block = 0
+
+    # Loop on blocks of data
     for i in range(0, ysize, blk_rows):
-        if i + blk_rows + 1 < ysize:
-            rows = blk_rows + 2
-        else:
-            rows = ysize - i
-        yoff = max(0, i - 1)
 
+        # Progress bar
+        nblock = (ysize // blk_rows) + 1
+        iter_block = iter_block + 1
+        progress_bar(nblock, iter_block)
+
+        # Extra lines at the bottom and top
+        extra_lines = win_size // 2
+
+        # Compute y offset and line numbers
+        # For the condition, think in terms of cell index (starting from 0),
+        # not cell number (starting from 1).
+        if (i + blk_rows + 2 * extra_lines - 1) < ysize:
+            rows = blk_rows + 2 * extra_lines
+        else:
+            rows = ysize - i + extra_lines
+        yoff = max(0, i - extra_lines)
+
+        # Read block data
         in_data = in_band.ReadAsArray(0, yoff, xsize, rows)
-        # defor
-        defor_data = np.zeros(in_data.shape, np.int8)
+        # defor (during first period)
+        defor_data = np.zeros(in_data.shape, int)
         defor_data[np.where(in_data == 1)] = 1
-        win_defor = scipy.ndimage.uniform_filter(
-            defor_data, size=win_size, mode="constant", cval=0)
-        # for
-        for_data = np.zeros(in_data.shape, np.int8)
-        for_data[np.where(in_data != 0)] = 1
-        win_for = scipy.ndimage.uniform_filter(
-            for_data, size=win_size, mode="constant", cval=0)
+        win_defor = scipy.ndimage.filters.uniform_filter(
+            defor_data, size=win_size, mode="constant", cval=0,
+            output=np.float64)
+        # for (start of first period)
+        for_data = np.zeros(in_data.shape, int)
+        w = np.where(in_data > 0)
+        for_data[w] = 1
+        win_for = scipy.ndimage.filters.uniform_filter(
+            for_data, size=win_size, mode="constant", cval=0,
+            output=np.float64)
         # percentage
-        out_data = np.rint(10000 * win_defor / win_for)
+        out_data = np.ones(in_data.shape, int)*65535
+        # w = np.where(win_for > 0)
+        out_data[w] = np.rint(10000 * (1 - (1 - win_defor[w] / win_for[w]) **
+                                       time_interval)).astype(int)
 
         if yoff == 0:
             out_band.WriteArray(out_data)
         else:
-            out_band.WriteArray(out_data[1:], 0, yoff + 1)
+            out_band.WriteArray(out_data[(extra_lines):], 0,
+                                yoff + extra_lines)
 
     out_band.FlushCache()
     out_band.ComputeStatistics(False)
     del out_ds, in_ds
+
+
+# Test
+ws = 7
+local_defor_rate(input_file="data/fcc123.tif",
+                 output_file="outputs/ldefrate_ws{}.tif".format(ws),
+                 win_size=ws,
+                 time_interval=5,
+                 blk_rows=100)
 
 # End
