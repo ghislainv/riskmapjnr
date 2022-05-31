63b854bb94ff87b250e73ce72bc9d99ce4ea68e7
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2022-05-31 18:10:37 +0200

    Advancing validation

--- validation.py
+++ validation.py
@@ -16,50 +16,56 @@ from osgeo import gdal
 import pandas as pd
 
 # Local application imports
-from misc import progress_bar, makeblock
+from misc import progress_bar, make_square
 
 
 # validation
-def validation(input_file,
-               output_file="defor_cat.tif",
-               nbins=30,
-               method="Equal Area",
-               blk_rows=128):
-    """Categorizing local deforestation rates.
+def validation(fcc_file, time_interval,
+               defor_cat_file, defrate_per_cat_file,
+               csize=300,
+               tab_file="validation_data.csv",
+               fig_file="pred_obs.png"):
+    """Validation of the deforestation risk map.
 
-    This function categorizes the deforestation risk from the map of
-    local deforestation rates. This function assumes that pixels with
-    zero deforestation risk have been previously identified (see
-    function ``set_defor_cat_zero()``). Three categorization methods
-    can be used, either "Equal Area", "Equal Interval", or "Natural
-    Breaks". When "Equal Area" is used, the classes with a risk > 0
-    have approximately the same surface area. When "Equal Interval" is
-    used, some risk classes will predominate in the risk map while
-    other classes will be present only in small areas. When "Natural
-    Breaks" is used, the data is normalized before running the slicing
-    algorithm.
+    This function computes the observed and predicted deforestion (in
+    ha) for the validation time period. Deforestion is computed for
+    spatial grid cells of a maximum size of 10km. Deforestation rate
+    estimates obtained with the ``defrate_per_cat`` function are used
+    to compute the predicted deforestation in each grid cell. The
+    function creates both a `.csv` file with the validation data and a
+    plot comparing predictions vs. observations. The function returns
+    the weighted Root Mean Squared Error (wRMSE, in hectares)
+    associated with the deforestation predictions.
 
-    :param input_file: Input raster file of local deforestation
-        rates. Deforestation rates are defined by integer values
-        between 0 and 10000 (ten thousand)). Pixels with zero
-        deforestation risk (beyond a certain distance from the forest
-        edge) have value 10001. This file is typically obtained with
-        function ``set_defor_cat_zero()``.
+    :param fcc_file: Input raster file of forest cover change at three
+        dates (123). 1: first period deforestation, 2: second period
+        deforestation, 3: remaining forest at the end of the second
+        period. No data value must be 0 (zero).
 
-    :param output_file: Output raster file with categories of
-        deforestation risk.
+    :param time_interval: Time interval (in years) for forest cover
+        change observations.
 
-    :param nbins: Number of deforestation risk categories (zero
-        risk class excluded). Default to 30.
+    :param defor_cat_file: Input raster file with categories of
+        spatial deforestation risk. This file is typically obtained
+        with function ``defor_cat()``.
 
-    :param method: Method used for categorizing. Either "Equal
-        Interval", "Equal Area", or "Natural Breaks".
+    :param defrate_per_cat_file: Path to the `.csv` input file with
+        estimates of deforestation rates per category of deforestation
+        risk. This file is typically obtained with function
+        ``defrate_per_cat()``.
 
-    :param blk_rows: If > 0, number of rows for computation by block.
+    :param csize: Spatial cell size in number of pixels. Must
+        correspond to a distance < 10 km. Default to 300 corresponding
+        to 9 km for a 30 m resolution raster.
 
-    :return: None. A raster file with deforestation categories will be
-        created (see ``output_file``). Data range from 0 to 30. Raster
-        type is Byte ([0, 255]). NoData value is set to 255.
+    :param tab_file: Path to the `.csv` output file with validation
+        data.
+
+    :param fig_file: Path to the `.png` output file for the
+        predictions vs. observations plot.
+
+    :return: the weighted Root Mean Squared Error (in hectares) of the
+        deforestation predictions.
 
     """
 
@@ -67,124 +73,99 @@ def validation(input_file,
     # Input raster
     # ==============================================================
 
-    # Get catzero (catzero) raster data
-    catzero_ds = gdal.Open(input_file)
-    catzero_band = catzero_ds.GetRasterBand(1)
-    # Raster size
-    xsize = catzero_band.XSize
-    ysize = catzero_band.YSize
+    # Get fcc raster data
+    fcc_ds = gdal.Open(fcc_file)
+    fcc_band = fcc_ds.GetRasterBand(1)
 
-    # Make blocks
-    blockinfo = makeblock(input_file, blk_rows=blk_rows)
-    nblock = blockinfo[0]
-    nblock_x = blockinfo[1]
-    x = blockinfo[3]
-    y = blockinfo[4]
-    nx = blockinfo[5]
-    ny = blockinfo[6]
-    print("Divide region in {} blocks".format(nblock))
+    # Get defor_cat raster data
+    defor_cat_ds = gdal.Open(defor_cat_file)
+    defor_cat_band = defor_cat_ds.GetRasterBand(1)
 
-    # ==============================================
-    # Categorical raster file for deforestation risk
-    # ==============================================
+    # Get defrate per cat
+    defrate_per_cat = pd.read_csv(defrate_per_cat_file)
+    cat_csv = defrate_per_cat["cat"].values
 
-    # Create categorical (cat) raster file for deforestation risk
-    driver = gdal.GetDriverByName("GTiff")
-    cat_ds = driver.Create(output_file, xsize, ysize, 1,
-                           gdal.GDT_Byte,
-                           ["COMPRESS=LZW", "PREDICTOR=2",
-                            "BIGTIFF=YES"])
-    cat_ds.SetProjection(catzero_ds.GetProjection())
-    cat_ds.SetGeoTransform(catzero_ds.GetGeoTransform())
-    cat_band = cat_ds.GetRasterBand(1)
-    cat_band.SetNoDataValue(255)
-
-    # -----------------
-    # Categorize
-    # -----------------
-
-    # Equal Interval
-    if method == "Equal Interval":
-        bin_size = round(10000 / nbins)
-        bins = [i * bin_size for i in range(nbins)]
-        bins = bins + [10000, 10001]
-
-    # Equal Area
-    if method == "Equal Area":
-        # Compute histogram
-        print("Compute histogram")
-        nvalues = 10000 + 1
-        counts = catzero_band.GetHistogram(-0.5, 10000.5, nvalues, 0, 0)
-        npix = sum(counts)
-        # Percentage
-        perc = np.array(counts) / npix
-        # Cumulative percentage
-        cum_perc = np.cumsum(perc)
-        # Correction of the approximation
-        cum_perc[-1] = 1.0
-        # Quantiles
-        q = [i * 1 / nbins for i in range(1, nbins + 1)]
-        # Bins
-        bins = [0]
-        # Loop on quantiles
-        for qi in q:
-            comp = (cum_perc <= qi)
-            sum_comp = np.sum(comp)
-            if sum_comp != 0:
-                bins.append(sum_comp - 1)
-        # Remove duplicate
-        bins = list(np.unique(bins))
-        # Add category 10001
-        bins.append(10001)
-
-    # Loop on blocks of data
-    for b in range(nblock):
-        # Progress bar
-        progress_bar(nblock, b + 1)
-        # Position
-        px = b % nblock_x
-        py = b // nblock_x
-        # Data
-        catzero_data = catzero_band.ReadAsArray(x[px], y[py], nx[px], ny[py])
-        # Categorize
-        cat_data = pd.cut(catzero_data.flatten(), bins=bins,
-                          labels=False, include_lowest=True)
-        cat_data = cat_data + 1
-        cat_data[np.isnan(cat_data)] = 255
-        cat_data[cat_data == (len(bins) - 1)] = 0
-        cat_data = cat_data.reshape(catzero_data.shape)
-        # Write to file
-        cat_band.WriteArray(cat_data, x[px], y[py])
-
-    # Compute statistics
+    # Number of deforestation categories
     print("Compute statistics")
-    cat_band.FlushCache()
-    cat_band.ComputeStatistics(False)
+    stats = defor_cat_band.ComputeStatistics(False)
+    n_cat = int(stats[1])  # Get the maximum
+    cat_raster = np.array([c + 1 for c in range(n_cat)])
 
+    # Check categories
+    if not np.array_equal(cat_csv, cat_raster):
+        msg = ("Categories in the 'defrate_per_cat_file' csv file do not"
+               "correspond to categories in the 'defor_cat_file' raster"
+               "file")
+        raise ValueError(msg)
+    else:
+        cat = cat_csv
+
+    # Pixel area (in unit square, eg. meter square)
+    gt = fcc_ds.GetGeoTransform()
+    pix_area = gt[1] * (-gt[5])
+
+    # Make square
+    squareinfo = make_square(fcc_file, csize)
+    nsquare = squareinfo[0]
+    nsquare_x = squareinfo[1]
+    x = squareinfo[3]
+    y = squareinfo[4]
+    nx = squareinfo[5]
+    ny = squareinfo[6]
+    print("Divide region in {} square cells".format(nsquare))
+
+    # Create a table to save the results
+    data = {"cell": list(range(nsquare)), "nfor_obs": 0,
+            "ndefor_obs": 0, "ndefor_pred": 0}
+    df = pd.DataFrame(data)
+
+    # Loop on squares
+    for s in range(nsquare):
+        # Progress bar
+        progress_bar(nsquare, s + 1)
+        # Position in 1D-arrays
+        px = s % nsquare_x
+        py = s // nsquare_x
+        # Observed forest cover and deforestation for validation period
+        fcc_data = fcc_band.ReadAsArray(x[px], y[py], nx[px], ny[py])
+        df.loc[s, "nfor_obs"] = np.sum(fcc_data > 1)
+        df.loc[s, "ndefor_obs"] = np.sum(fcc_data == 2)
+        # Predicted deforestation for validation period
+        defor_cat_data = defor_cat_band.ReadAsArray(
+            x[px], y[py], nx[px], ny[py])
+        defor_cat = pd.Categorical(defor_cat_data.flatten(), categories=cat)
+        defor_cat_count = defor_cat.value_counts().values
+        df.loc[s, "ndefor_pred"] = np.sum(defor_cat_count *
+                                          defrate_per_cat["rate"].values)
     # Dereference drivers
-    cat_band = None
-    del cat_ds, catzero_ds
+    del fcc_ds, defor_cat_ds
+
+    # Select cells with forest cover > 0
+    df = df[df["nfor_obs"] > 0]
+    if df.shape[0] < 1000:
+        msg = ("Number of cells with forest cover > 0 ha is < 1000."
+               "Please decrease the spatial cell size 'csize'.")
+        raise ValueError(msg)
+
+    # Compute areas in ha
+    df["nfor_obs_ha"] = df["nfor_obs"] * pix_area / 10000
+    df["ndefor_obs_ha"] = df["ndefor_obs"] * pix_area / 10000
+    df["ndefor_pred_ha"] = df["ndefor_pred"] * pix_area / 10000
+
+    # Export the table of results
+    df.to_csv(tab_file, sep=",", header=True,
+              index=False, index_label=False)
 
     return None
 
 
-# # Test
-# input_file = "outputs/defor_cat_zero.tif"
-# output_file = "outputs/defor_cat.tif"
-# nbins = 30
-# method = "Equal Area"
-# blk_rows = 128
-
-# defor_cat(input_file,
-#           output_file="outputs/defor_cat_equal_interval.tif",
-#           nbins=30,
-#           method="Equal Interval",
-#           blk_rows=128)
-
-# defor_cat(input_file,
-#           output_file="outputs/defor_cat_equal_area.tif",
-#           nbins=30,
-#           method="Equal Area",
-#           blk_rows=128)
+# Test
+fcc_file = "data/fcc123.tif"
+time_interval = 10
+defor_cat_file = "outputs/defor_cat.tif"
+defrate_per_cat_file = "outputs/defrate_per_cat.csv"
+csize = 300
+tab_file = "outputs/validation_data.csv"
+fig_file = "outputs/pred_obs.png"
 
 # End
